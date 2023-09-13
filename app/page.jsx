555594efa820bfed9559ea6bb834c53210d7cfdd
Timiwade97 <timiwade97@gmail.com>
2023-09-13 21:02:11 +0100

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -20,7 +20,7 @@ const Home = () => {
         <div
           data-aos="fade-right"
           data-aos-delay="300"
-          className="flex-1 flex-col p-4 text-center md:p-11 md:pt-[200px] md:text-left"
+          className="flex-1 flex-col p-4 text-center md:p-11 md:pt-[100px] md:text-left"
         >
           <h6 className="pb-4 text-4xl">WELCOME TO:</h6>
           <h1 className="pb-4 text-8xl">
@@ -125,117 +125,85 @@ const Home = () => {
         </div>
       </section>
 
-      {/* <section className="bg-black pb-[100px] pt-[100px]">
+      <section className="bg-black pb-[100px] pt-[100px]">
         <div className="container mx-auto flex w-full flex-col  text-center text-white">
-          <h1 className="pb-8 text-6xl">ROADMAP</h1>
-          <div className="flex w-full flex-col md:flex-row">
+          <div className="flex flex-col md:flex-row">
             <div
               data-aos="zoom-in-left"
               data-aos-delay="300"
-              className="roadmap-card m-3 flex-1 text-center"
+              className=" m-3 flex-1 text-center"
             >
-              <h1 className="pb-10 text-3xl">
-                Phase 1: Project Launch and Community Building
-              </h1>
-              <p>
-                Project Inception: Start by defining the $GREG project's concept
-                and goals, assemble a core team, and secure initial funding
-                through private sales. Develop the whitepaper and create an
-                online presence.
-                <br />
-                <br />
-                Token Generation Event: Conduct a successful TGE to distribute
-                $GREG tokens, list on crypto exchanges, and launch the official
-                website and social media channels. Build a growing and engaged
-                community.
-                <br />
-                <br />
-                Community Engagement: Actively interact with the community via
-                various platforms, release teasers, and host events to build
-                excitement. Reward early supporters to foster enthusiasm and
-                participation.
-              </p>
+              <Image
+                src="/assets/pic1.jpg"
+                alt="Logo"
+                width={300}
+                height={300}
+                className="mx-auto w-full object-contain"
+              />
             </div>
 
             <div
               data-aos="zoom-in-left"
               data-aos-delay="300"
-              className="roadmap-card m-3 flex-1 text-center"
+              className=" m-3 flex-1 text-center"
             >
-              <h1 className="pb-10 text-3xl">
-                Phase 2: NFT Launch and Community Engagement
-              </h1>
-              <p>
-                NFT Collection Development: Collaborate with artists to create
-                the initial NFT collection inspired by Greg's adventures,
-                announce unique features, and plan an NFT presale to attract
-                early adopters.
-                <br />
-                <br />
-                NFT Launch and Meme Contest: Release the first NFT batch,
-                conduct a meme contest to engage the community, and actively
-                promote NFTs through social media for wider adoption. Community
-                Governance:
-                <br />
-                <br />
-                Implement community governance mechanisms like a DAO, allow
-                token holders to influence project decisions through votes, and
-                maintain community engagement with NFT releases and contests.
-              </p>
+              <Image
+                src="/assets/pic2.jpg"
+                alt="Logo"
+                width={300}
+                height={300}
+                className="mx-auto w-full object-contain"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col md:flex-row">
+            <div
+              data-aos="zoom-in-left"
+              data-aos-delay="300"
+              className=" m-3 flex-1 text-center"
+            >
+              <Image
+                src="/assets/pic3.jpg"
+                alt="Logo"
+                width={300}
+                height={300}
+                className="mx-auto w-full object-contain"
+              />
             </div>
 
             <div
               data-aos="zoom-in-left"
               data-aos-delay="300"
-              className="roadmap-card m-3 flex-1 text-center"
+              className=" m-3 flex-1 text-center"
             >
-              <h1 className="pb-10 text-3xl">
-                Phase 3: Partnerships and Charity Initiatives
-              </h1>
-              <p>
-                Partnerships: Seek partnerships with crypto projects,
-                influencers, or brands that align with $GREG's vision,
-                collaborate on unique NFTs or events, and leverage partnerships
-                for increased visibility and growth.
-                <br />
-                <br />
-                Charity Initiatives: Dedicate a portion of project earnings to
-                charitable initiatives in line with "Diary of a Wimpy Kid"
-                themes, announce and promote these initiatives to the community,
-                and involve the community in selecting beneficiaries to foster a
-                sense of ownership.
-              </p>
+              <Image
+                src="/assets/pic4.jpg"
+                alt="Logo"
+                width={300}
+                height={300}
+                className="mx-auto w-full object-contain"
+              />
             </div>
+          </div>
 
+          <div className="flex flex-col md:flex-row">
             <div
               data-aos="zoom-in-left"
               data-aos-delay="300"
-              className="roadmap-card m-3 flex-1 text-center"
+              className=" m-3 flex-1 text-center"
             >
-              <h1 className="pb-10 text-3xl">
-                Phase 4: Long-Term Sustainability and Growth
-              </h1>
-              <p>
-                Ongoing Development: Continue releasing new NFT collections,
-                explore ecosystem expansion possibilities like marketplaces or
-                metaverse integration, and enhance community governance features
-                based on feedback.
-                <br />
-                <br />
-                Sustained Growth: Adapt and evolve the project based on market
-                trends and community feedback, maintain a strong focus on
-                community engagement and NFT development, and explore additional
-                partnerships and collaborations for sustained long-term growth.
-                <br />
-                <br />
-                Beyond Month 30: Continue evolving the project, explore new
-                opportunities, and ensure its longevity by staying responsive to
-                market dynamics and community needs.
-              </p>
+              <Image
+                src="/assets/pic5.jpg"
+                alt="Logo"
+                width={300}
+                height={300}
+                className="mx-auto w-full object-contain"
+              />
             </div>
           </div>
         </div>
-      </section> */}
+      </section>
     </>
   );
 };
